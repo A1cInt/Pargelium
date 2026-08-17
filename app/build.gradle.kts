f6bbbe647298123220ab9b5654d1b8f5bdc61d78
AlcInt <148353020+A1cInt@users.noreply.github.com>
2026-08-18 02:14:45 +0300

    Fix italian and France

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.alcint.pargelium"
         minSdk = 31
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
         ndkVersion = "30.0.15729638"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
