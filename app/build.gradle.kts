582c9d0c4e6abb8e96a5ef5191a985a22951f2ca
AlcInt <148353020+A1cInt@users.noreply.github.com>
2026-08-20 15:18:19 +0300

    Add a lot of new languages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.alcint.pargelium"
         minSdk = 31
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.2"
         ndkVersion = "30.0.15729638"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
